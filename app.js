29f69817e317c15cd158fac3f2ee227ba5ee16b7
UTKARSH <utkarsh1480759@gmail.com>
2026-04-23 03:18:26 +0530

    chore: make project vercel-ready

--- app.js
+++ app.js
@@ -27,3 +27,5 @@ app.use('/blog', blogRoute);
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
 });
+
+module.exports = app;
